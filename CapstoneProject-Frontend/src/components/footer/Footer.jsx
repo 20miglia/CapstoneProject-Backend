0653a29b89943382a65e39d0c74a99b3f5920e2b
Gianluca Ventimiglia <20migliagianluca@gmail.com>
2025-06-20 12:01:13 +0200

    Upgrade-6

--- CapstoneProject-Frontend/src/components/footer/Footer.jsx
+++ CapstoneProject-Frontend/src/components/footer/Footer.jsx
@@ -6,12 +6,14 @@ import './StyleFooter.css'
 
 function Footer() {
     return (
+        
         <footer className="setDown text-white text-center py-2">
             <div className="container">
                 <p className="mb-0">© 2025 Il Covo del Re. All rights reserved.</p>
                 <p className="mb-0">Developed by Ventimiglia Gianluca</p>
             </div>
         </footer>
+       
     )
 }
 
